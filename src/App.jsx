b831f41c266a5208b8458b321e36e4a6ffc1ffff
ukaran11 <ukaran11@gmail.com>
2021-08-29 16:12:05 +0530

    Utsav | Added multiple cards and responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import {PhotoCamera} from '@material-ui/icons';
 
 import useStyles from './styles';
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const App = () => {
     const classes = useStyles();
 
@@ -45,9 +47,29 @@ const App = () => {
               </div>
               <Container className={classes.cardGrid} maxWidth="md">
                   <Grid container spacing={4}>
-                      <Grid item>
-                          <Card></Card>
-                      </Grid>
+                      {cards.map((card) => (
+                        <Grid item key={card} xs={12} sm={6} md={4}>
+                            <Card className={classes.card}>
+                                <CardMedia
+                                    className={classes.cardMedia}
+                                    image="https://source.unsplash.com/random"
+                                    title="Image title"
+                                    />
+                                <CardContent className={classes.cardContent}>
+                                        <Typography gutterBottom variant="h5">
+                                            Heading
+                                        </Typography>
+                                        <Typography>
+                                            This is a media card. Toy can use this section to describe the content.
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small" color="primary">View</Button>
+                                        <Button size="small" color="primary">Edit</Button>
+                                    </CardActions>
+                            </Card>
+                        </Grid>
+                      ))}
                   </Grid>
               </Container>
               
--- src/styles.js
+++ src/styles.js
@@ -10,6 +10,20 @@ const useStyles = makeStyles((theme) => ({
     },
     button: {
         marginTop: '40px'
+    },
+    cardGrid: {
+        padding: '20px 0'
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    cardMedia: {
+        paddingTop: '56.25%'
+    },
+    cardContent: {
+        flexGrow: 1
     }
 }));
 
